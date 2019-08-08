a2e1d4a859fcc80c7aa65977a7fd5f13a4708b57
Zhenning Ruan <rzning@outlook.com>
2019-08-09 00:59:44 +0800

    Add typedoc.json

--- monaco.d.ts
+++ monaco.d.ts
@@ -3,5 +3,20 @@ declare namespace monaco {
 }
 
 declare namespace monaco.editor {
-    
+
+  /**
+   * 编辑器
+   */
+  export interface IEditor {
+
+  }
+
+}
+
+declare namespace monaco.languages {
+
+}
+
+declare namespace monaco.worker {
+
 }
