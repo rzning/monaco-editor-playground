b4ffd94525ee801be4bf1032f7e0a325d54c74b5
Zhenning Ruan <rzning@outlook.com>
2019-09-21 00:00:00 +0800

    Update monaco.editor.TextModel

--- typings/editor/common/model/textModel.d.ts
+++ typings/editor/common/model/textModel.d.ts
@@ -1,6 +1,7 @@
 // todo...
 
 /// <reference types="../../../base/common/lifecycle" />
+/// <reference types="../model" />
 
 declare namespace monaco.editor {
 
@@ -11,8 +12,42 @@ declare namespace monaco.editor {
     
     public readonly id: string; // :240
     public readonly isForSimpleWidget: boolean;
+    private _attachedEditorCount: number;
+    private _buffer: ITextBuffer;
+    private _options: TextModelResolvedOptions;
+  
+    /**
+     * 
+     */
+    public setValue(value: string): void; // :374
+
+    public setValueFromTextBuffer(textBuffer: ITextBuffer): void; // :401
 
     // public get uri(): Uri; // :556
     public uri: Uri;
+
+    //#region Options :560
+
+    public getOptions(): TextModelResolvedOptions;
+
+    public getFormattingOptions(): FormattingOptions;
+
+    //#endregion
+
+    //#region Reading :650
+
+    public getVersionId(): number;
+
+    /**
+     * `this._buffer.mightContainRTL();`
+     */
+    public mightContainRTL(): boolean;
+
+    public mightContainNonBasicASCII(): boolean;
+
+    public getAlternativeVersionId(): number;
+
+    //#endregion
+
   }
 }
